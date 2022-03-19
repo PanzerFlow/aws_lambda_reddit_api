f5a05f2c5de56c6e4b16a019d184834347fdb2ea
Ubuntu <ubuntu@ip-172-31-84-146.ec2.internal>
2022-03-19 18:00:24 +0000

    Updating the API calls

--- RedditAPI.py
+++ RedditAPI.py
@@ -37,7 +37,7 @@ def create_temp_auth():
 #Attempt to write a function to parse this json result and save to csv. Too hard to treat the text the posts.
 def reddit_json_to_csv(input_json_text:String,result_csv_path:String):
     """
-       A function used to parse the result of reddit api.
+       A function for parsing the result of reddit api.
         Input
         ----------
         input_json_text : str
@@ -48,26 +48,27 @@ def reddit_json_to_csv(input_json_text:String,result_csv_path:String):
     with open(result_csv_path, 'w', newline='') as csvfile:
 
         writer = csv.writer(csvfile, delimiter='|')
-        writer.writerow(['post_title','author','post_category','post_time_utc','last_commet_time_utc','post_text'])
+        writer.writerow(["title","id","subreddit_id","author","link_flair_text","created_at_utc","post_text","post_score"])
         for post in input_json_text['data']['children']:
             title = post['data']['title']
+            id = post['data']['id']
+            subreddit_id = post['data']['subreddit_id']
             author = post['data']['author']
-            link_flair_text = post['data']['link_flair_text']
-            #created_at_utc = post['data']['collections'][0]['created_at_utc'] #Not all post have these...
-            #last_update_utc = post['data']['collections'][0]['last_update_utc'] #Not all post have these...
-            selftext = post['data']['selftext']
-            writer.writerow([title,author,link_flair_text,selftext])
+            link_flair_text = post['data']['link_flair_text']         
+            created_at_utc = post['data']['created_utc']
+            post_text = post['data']['selftext']
+            post_score = post['data']['score']
+            writer.writerow([title,id,subreddit_id,author,link_flair_text,created_at_utc,post_text,post_score])
     
 
 
 api_headers = create_temp_auth()
 #api_headers = {'User-Agent': 'DataEngSubscraper/0.0.1', 'Authorization': 'bearer 20340382-PD7s2j2eidebvVFNJfCMJGyQEP1WrQ'}
 
-res = requests.get("https://oauth.reddit.com/r/dataengineering/hot",
-                   headers=api_headers)
+res = requests.get("https://oauth.reddit.com/r/dataengineering/new",
+                   headers=api_headers, params={'limit': '100'})
 
 res_json = res.json()
-
 reddit_json_to_csv(res_json,'./reddit_api_result.csv')
     
 
